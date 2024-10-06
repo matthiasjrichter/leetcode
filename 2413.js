4ea033fbd66a9a29c35fc8f6516c5daadb3457b3
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-10-06 09:10:21 +0200

    new file:   2413.js

---
+++ 2413.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/smallest-even-multiple/description/
+// 
+// 2413. Smallest Even Multiple
+// 
+// Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+//  
+// 
+// Example 1:
+// 
+// Input: n = 5
+// Output: 10
+// Explanation: The smallest multiple of both 5 and 2 is 10.
+// Example 2:
+// 
+// Input: n = 6
+// Output: 6
+// Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.
+//  
+// 
+// Constraints:
+// 
+// 1 <= n <= 150
+// 
+// /**
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var smallestEvenMultiple = function(n) {
+//     
+// };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const smallestEvenMultiple = (n) => n % 2 === 0 ? n : n * 2;
