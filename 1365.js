742f01018408d66b59e1407b1f14ca448e880e1d
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2024-10-07 19:05:06 +0200

    new file:   1365.js

---
+++ 1365.js
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/description/
+//
+// 1365. How Many Numbers Are Smaller Than the Current Number
+//
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+//
+// Return the answer in an array.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation:
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1).
+// For nums[3]=2 there exist one smaller number than it (1).
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+// Example 2:
+//
+// Input: nums = [6,5,4,8]
+// Output: [2,1,0,3]
+// Example 3:
+//
+// Input: nums = [7,7,7,7]
+// Output: [0,0,0,0]
+//
+//
+// Constraints:
+//
+// 2 <= nums.length <= 500
+// 0 <= nums[i] <= 100
+//
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// var smallerNumbersThanCurrent = function(nums) {
+//
+// };
+
+//Parameters: array of numbers
+//Return: array of numbers
+//Examples: Input: nums = [6,5,4,8] -> Output: [2,1,0,3]; Input: nums = [7,7,7,7] -> Output: [0,0,0,0]
+//Pseudocode: create new sorted array. Loop through nums and get index of current element in sorted array.
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function (nums) {
+  let sorted = [...nums].sort((a, b) => a - b);
+  return nums.map((e) => sorted.indexOf(e));
+};
+
+//test
+smallerNumbersThanCurrent([6, 5, 4, 8]); //[2,1,0,3]
+
